c6303ab5cb23a2f29ce3433d26351a2dfe1dba2a
wishalF <wishalfakira@hotmail.com>
2025-02-11 23:38:49 +0100

    add counter and date

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const [task, setTask] = useState({ text: "", done: false });
+  const [task, setTask] = useState({ text: "", done: false, date: "" });
 
   // Function to remove a specific task
   const removeTask = (index) => {
@@ -13,29 +13,43 @@ function App() {
     });
   };
 
-  // Function to remove all tasks
-  const removeAll = () => {
-    setTasks([]);
-  };
-
-  // Function to handle the task text input
-  const handleAddTask = () => {
-    if (task.text.trim() === "") return; // Don't add empty tasks
-    setTasks([...tasks, { ...task }]); // Add new task
-    setTask({ text: "", done: false }); // Reset task input
-  };
-
   // Function to toggle task status (done/undone)
   const toggleTaskStatus = (index) => {
     setTasks((prevTasks) =>
       prevTasks.map((t, i) => (i === index ? { ...t, done: !t.done } : t))
     );
   };
+  // Function to handle the task text input
+  const handleAddTask = () => {
+    if (task.text.trim() === "") return; // Don't add empty tasks
+    setTasks([
+      ...tasks,
+      {
+        ...task,
+        date: new Date().toLocaleString("nl-NL", {
+          weekday: "long",
+          month: "long",
+          day: "numeric",
+        }),
+      },
+    ]); // Add new task
+    setTask({
+      text: "",
+      done: false,
+    }); // Reset task input
+  };
+  // Function to remove all tasks
+  const removeAll = () => {
+    setTasks([]);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>To-do app</h1>
+        {tasks?.[0] && (
+          <h2>You have {tasks.filter((t) => !t.done).length} tasks left</h2>
+        )}
       </header>
       <div className="todo">
         <div className="todo__input">
@@ -50,25 +64,37 @@ function App() {
           </button>
         </div>
         <div className="todo__list">
-          <ul>
-            {tasks.map((t, index) => (
-              <li key={index} className={t.done ? "task--false" : "task--done"}>
-                {t.text}
-                <button
-                  onClick={() => toggleTaskStatus(index)}
-                  className="button button--add"
+          {tasks.length > 0 ? (
+            <ul>
+              {tasks.map((t, index) => (
+                <li
+                  key={index}
+                  className={t.done ? "task task--false" : "task task--done"}
                 >
-                  {t.done ? "Undo" : "Done"}
-                </button>
-                <button
-                  onClick={() => removeTask(index)}
-                  className="button button--remove"
-                >
-                  Delete this task
-                </button>
-              </li>
-            ))}
-          </ul>
+                  <div className="todo__column">
+                    <span>{t.text}</span>{" "}
+                    <div className="todo__date">{t.date}</div>
+                  </div>
+                  <div className="todo__buttons">
+                    <button
+                      onClick={() => toggleTaskStatus(index)}
+                      className="button button--add"
+                    >
+                      {t.done ? "Undo" : "Done"}
+                    </button>
+                    <button
+                      onClick={() => removeTask(index)}
+                      className="button button--remove"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No tasks added</p>
+          )}
         </div>
       </div>
       {tasks?.[0] && (
